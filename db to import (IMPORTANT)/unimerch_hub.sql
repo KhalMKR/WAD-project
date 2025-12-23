7fc07c2396a717d0bfdd2af08eef1b7bf6fe4795
Khal <muhdkhalishreeza@gmail.com>
2025-12-23 09:15:17 +0800

    replace the sql file with the new one, with some more data

--- db to import (IMPORTANT)/unimerch_hub.sql
+++ db to import (IMPORTANT)/unimerch_hub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2025 at 07:47 AM
+-- Generation Time: Dec 23, 2025 at 02:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,8 +31,48 @@ CREATE TABLE `orders` (
   `orderID` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
   `totalAmount` decimal(10,2) NOT NULL,
-  `orderDate` timestamp NOT NULL DEFAULT current_timestamp()
+  `orderDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fullName` varchar(200) NOT NULL DEFAULT '',
+  `phone` varchar(50) NOT NULL DEFAULT '',
+  `address` varchar(400) NOT NULL DEFAULT '',
+  `paymentMethod` varchar(100) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`orderID`, `userID`, `totalAmount`, `orderDate`, `fullName`, `phone`, `address`, `paymentMethod`) VALUES
+(1, 3, 40.00, '2025-12-23 00:36:28', 'khal', '011-29180129', 'kuching', 'Bank Transfer'),
+(2, 3, 30.00, '2025-12-23 00:44:58', 'khal', '011-21231311', 'Betong', 'Card'),
+(3, 4, 15.00, '2025-12-23 00:55:07', 'Beta', '011-98798722', 'Lahad Datu', 'Card'),
+(4, 4, 85.00, '2025-12-23 01:12:24', 'Afiq', '013-71271264', 'California', 'E-Wallet');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `itemID` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  `productID` int(11) DEFAULT NULL,
+  `name` varchar(200) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`itemID`, `orderID`, `productID`, `name`, `price`, `quantity`) VALUES
+(1, 1, 3, 'UniMerch Cap', 30.00, 1),
+(2, 1, 4, 'Bracelet', 10.00, 1),
+(3, 2, 3, 'UniMerch Cap', 30.00, 1),
+(4, 3, 2, 'FCSIT Lanyard', 15.00, 1),
+(5, 4, 1, 'UNIMAS Hoodie', 85.00, 1);
 
 -- --------------------------------------------------------
 
@@ -80,7 +120,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`userID`, `fullName`, `email`, `password`, `userType`) VALUES
 (1, 'Admin', 'admin@unimhub.com', '$2y$10$FUG2SXSlNiggezw6AaTScOtMnItMWC4sNNPg0GiXGc1WM33nBd.7q', 'admin'),
 (2, 'chung', 'chian@mail.com', '$2y$10$Cm47nj4Ah76UOJ1YWbau1u0ySPjVBIv6dGqYSwR.0nkzT0MYfEvEK', 'user'),
-(3, 'khal', 'khal@mail.com', '$2y$10$E0h9ul.hQLtz2fh/3qzI6e3bFj/bsLbbnRpXwUHaF6PoO5v3AT3uq', 'user');
+(3, 'khal', 'khal@mail.com', '$2y$10$E0h9ul.hQLtz2fh/3qzI6e3bFj/bsLbbnRpXwUHaF6PoO5v3AT3uq', 'user'),
+(4, 'Beta', 'beta@mail.com', '$2y$10$Rg3eAahwH.djsBrgAgQWuOIHTr.T7Zgepw31D0I4788tEIbjfIhqm', 'user');
 
 --
 -- Indexes for dumped tables
@@ -93,6 +134,13 @@ ALTER TABLE `orders`
   ADD KEY `userID` (`userID`);
 
 --
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`itemID`),
+  ADD KEY `orderID` (`orderID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -103,10 +151,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
